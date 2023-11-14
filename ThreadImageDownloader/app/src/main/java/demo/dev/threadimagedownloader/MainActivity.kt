a6a17b607b56494829bcef220fa807706bcae4f4
Ashif Mujtoba <nmujtoba@gmail.com>
2023-11-14 13:51:15 +0600

    thread learning done and initial commit

---
+++ ThreadImageDownloader/app/src/main/java/demo/dev/threadimagedownloader/MainActivity.kt
@@ -0,0 +1,25 @@
+package demo.dev.threadimagedownloader
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import demo.dev.threadimagedownloader.ui.theme.ThreadImageDownloaderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+
+}
+
